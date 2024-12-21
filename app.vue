329589c1cf8c1af2174940fe878d5f3deade4e70
sanjarbarakayev <genius.s.1799@gmail.com>
2024-12-22 00:43:38 +0500

    feat: add starter seo config

--- app.vue
+++ app.vue
@@ -3,3 +3,26 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+const SITE_TITLE = "Nuxt app";
+const SITE_DESCRIPTION = "Nuxt app description";
+const SITE_IMAGE = "https://example.com/image.jpg";
+
+useSeoMeta({
+  title: SITE_TITLE,
+  description: SITE_DESCRIPTION,
+  keywords: "home, page",
+  ogTitle: SITE_TITLE,
+  ogDescription: SITE_DESCRIPTION,
+  ogUrl: "https://example.com",
+  ogType: "website",
+  ogImage: SITE_IMAGE,
+  twitterTitle: SITE_TITLE,
+  twitterDescription: SITE_DESCRIPTION,
+  twitterCard: "summary_large_image",
+  twitterImage: SITE_IMAGE,
+  twitterSite: "@example",
+  twitterCreator: "@example",
+});
+</script>
