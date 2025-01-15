ff72004a34aee40aa84bff03cc0d3fe9061acec0
sanjarbarakayev <genius.s.1799@gmail.com>
2025-01-15 23:50:08 +0500

    feat: add footer

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,3 +1,211 @@
 <template>
-  <footer>Footer</footer>
+  <footer
+      class="flex flex-col items-center px-20 pt-10 pb-2 mt-16 w-full bg-neutral-900 max-md:px-5 max-md:mt-10 max-md:max-w-full"
+      role="contentinfo"
+      aria-label="Site footer"
+  >
+    <div class="flex flex-col w-full max-w-[1184px] max-md:max-w-full">
+      <div class="max-md:max-w-full">
+        <div class="flex gap-5 max-md:flex-col">
+          <div class="flex flex-col w-[35%] max-md:ml-0 max-md:w-full">
+            <div
+                class="flex flex-col grow mt-3 text-sm leading-5 text-white max-md:mt-10"
+            >
+              <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/41ccfdd9b43e3ba405e4a2566897d3c2585da429da44c7958e0baf3439a180b2?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+                  class="object-contain max-w-full aspect-[3.52] w-[264px]"
+                  alt="Turan Machinery Logo"
+              />
+              <p class="mt-5">
+                Настройтесь на новую эпоху бурения. На подходе революционные
+                инновации и расширенные функциональности, разработанные с учетом
+                вашей важной работы
+              </p>
+            </div>
+          </div>
+          <nav
+              class="flex flex-col ml-5 w-[19%] max-md:ml-0 max-md:w-full"
+              aria-label="Main navigation"
+          >
+            <ul
+                class="flex flex-col self-stretch my-auto text-base font-semibold leading-tight text-right text-white max-md:mt-10"
+            >
+              <li><a href="/" class="hover:text-opacity-80">Главная</a></li>
+              <li class="mt-4">
+                <a href="/about" class="hover:text-opacity-80">О компании</a>
+              </li>
+              <li class="mt-4">
+                <a href="/catalog" class="hover:text-opacity-80"
+                >Каталог спецтехники</a
+                >
+              </li>
+              <li class="mt-4">
+                <a href="/service" class="hover:text-opacity-80">Сервис</a>
+              </li>
+              <li class="mt-4">
+                <a href="/contacts" class="hover:text-opacity-80">Контакты</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="flex flex-col ml-5 w-[46%] max-md:ml-0 max-md:w-full">
+            <div class="flex flex-col w-full max-md:mt-10">
+              <address
+                  class="flex gap-3.5 items-center p-3 w-full rounded-xl border border-solid border-white border-opacity-20"
+              >
+                <div
+                    class="flex gap-2.5 items-center self-stretch p-2 my-auto rounded-lg bg-green-950 h-[38px] w-[38px]"
+                    aria-hidden="true"
+                >
+                  <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/52938ea19077a1705aba9540568bb8ac39d4ea4ce8af69abe0fa4bd707a92f6c?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+                      class="object-contain self-stretch my-auto aspect-square w-[22px]"
+                      alt=""
+                  />
+                </div>
+                <div
+                    class="flex flex-col justify-center self-stretch my-auto leading-tight min-w-[240px]"
+                >
+                  <div class="text-sm text-white text-opacity-60">Адрес:</div>
+                  <div
+                      class="flex gap-1.5 items-center text-sm font-medium text-white"
+                  >
+                    <div class="self-stretch my-auto">
+                      г.Ташкент, Учтепинский р-н, 23-квартал,53
+                    </div>
+                    <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/9c0e172fd267a41c9f3b187c6766b2220fa98ca0b44ac307637960b8fd6d16ea?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+                        class="object-contain shrink-0 self-stretch my-auto aspect-[1.07] w-[15px]"
+                        alt=""
+                    />
+                  </div>
+                </div>
+              </address>
+              <div
+                  class="flex gap-3.5 items-center p-3 mt-2 w-full rounded-xl border border-solid border-white border-opacity-20"
+              >
+                <div
+                    class="flex gap-2.5 items-center self-stretch p-2 my-auto rounded-lg bg-green-950 h-[38px] w-[38px]"
+                    aria-hidden="true"
+                >
+                  <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/ca5cebd3bed45819020c7e2549bf3ddc41a83f19475ce29f40eda0eb037d185b?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+                      class="object-contain self-stretch my-auto aspect-square w-[22px]"
+                      alt=""
+                  />
+                </div>
+                <div
+                    class="flex flex-col justify-center self-stretch my-auto leading-tight"
+                >
+                  <div class="text-sm text-white text-opacity-60">Телефон:</div>
+                  <a
+                      href="tel:+998998949323"
+                      class="text-sm font-medium text-white hover:text-opacity-80"
+                  >
+                    +998 (99) 894 93 23
+                  </a>
+                </div>
+              </div>
+              <div
+                  class="flex gap-3.5 items-center p-3 mt-2 w-full rounded-xl border border-solid border-white border-opacity-20"
+              >
+                <div
+                    class="flex gap-2.5 items-center self-stretch p-2 my-auto rounded-lg bg-green-950 h-[38px] w-[38px]"
+                    aria-hidden="true"
+                >
+                  <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/f8dfe5a52a994e84e08230012356bb12384850c9a9fd244eb8f7c6e223d64954?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+                      class="object-contain self-stretch my-auto aspect-square w-[22px]"
+                      alt=""
+                  />
+                </div>
+                <div class="flex flex-col justify-center self-stretch my-auto">
+                  <div class="text-sm leading-tight text-white text-opacity-60">
+                    Электронная почта:
+                  </div>
+                  <a
+                      href="mailto:info@turanmachinery.uz"
+                      class="text-sm font-medium text-white hover:text-opacity-80"
+                  >
+                    info@turanmachinery.uz
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+          class="flex shrink-0 mt-10 h-px bg-white bg-opacity-10 max-md:max-w-full"
+          role="separator"
+      ></div>
+      <div
+          class="flex flex-wrap gap-5 justify-between mt-4 w-full max-md:max-w-full"
+      >
+        <div class="my-auto text-sm leading-snug text-white text-opacity-60">
+          Copyright 2024 - "Turan Machinery" LLC
+        </div>
+        <div
+            class="flex gap-2 justify-center items-center"
+            role="list"
+            aria-label="Social media links"
+        >
+          <a
+              href="#"
+              class="social-link"
+              aria-label="Visit our Facebook page"
+              role="listitem"
+          >
+            <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/46426fb7097e5ed005a4b63c374d5ac7d4aa5a495c099cec68dab1572d4f2627?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+                class="object-contain shrink-0 self-stretch my-auto rounded-md aspect-square w-[31px]"
+                alt=""
+            />
+          </a>
+          <a
+              href="#"
+              class="social-link"
+              aria-label="Visit our Instagram page"
+              role="listitem"
+          >
+            <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/5c2db62d6e621aa4b6f5a283a979093252cd2ec2237892d4484a00ad4de8e768?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+                class="object-contain shrink-0 self-stretch my-auto rounded-md aspect-square w-[31px]"
+                alt=""
+            />
+          </a>
+          <a
+              href="#"
+              class="social-link"
+              aria-label="Visit our LinkedIn page"
+              role="listitem"
+          >
+            <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/24305ddaa99d90a41df1394d8c6163445726f2e454a1eee08bf490903ed213bb?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+                class="object-contain shrink-0 self-stretch my-auto rounded-md aspect-square w-[31px]"
+                alt=""
+            />
+          </a>
+        </div>
+        <div
+            class="flex gap-4 justify-center items-center self-start text-sm leading-snug text-white text-opacity-60"
+        >
+          <div class="self-stretch my-auto">Разработано компанией</div>
+          <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/d7b3550001d6813c0eeb5f9658ee4edf5337cc774225b0ddbad4fdca8f215cf5?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+              class="object-contain shrink-0 self-stretch my-auto aspect-[3.52] w-[95px]"
+              alt="Developer company logo"
+          />
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
