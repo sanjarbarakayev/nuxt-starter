861907849cfc4f6ca142922c03ed3dba393d926b
sanjarbarakayev <genius.s.1799@gmail.com>
2025-01-15 23:51:01 +0500

    feat: add home partners section

---
+++ components/Home/Partners.vue
@@ -0,0 +1,98 @@
+<template>
+  <section
+    class="flex flex-col mt-9 w-full max-md:max-w-full"
+    aria-labelledby="partners-heading"
+  >
+    <div class="flex flex-col self-start mt-12 max-md:mt-10 max-md:max-w-full">
+      <h2
+        id="partners-heading"
+        class="text-4xl font-extrabold leading-tight text-green-950 max-md:max-w-full"
+      >
+        Наши партнеры
+      </h2>
+      <p class="mt-2 text-sm leading-tight text-neutral-900 max-md:max-w-full">
+        Партнеры и клиенты компании
+      </p>
+    </div>
+    <div class="flex gap-4 items-start max-md:max-w-full" role="list">
+      <div role="listitem">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/fc3df40f66dd9275da1be283ad117f8fc2a838e314f163bf315f54b87aa3168e?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+          alt="Partner company logo"
+          class="object-contain shrink-0 rounded-lg aspect-[1.21] w-[119px]"
+        />
+      </div>
+      <div
+        role="listitem"
+        class="flex overflow-hidden flex-col justify-center px-8 py-10 rounded-lg bg-neutral-100 w-[184px] max-md:px-5"
+      >
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/5e8f9168023036653fa22accfecdcfb2cc31f9cc7399916a778c2adf6d11e6fa?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+          alt="Partner company logo"
+          class="object-contain aspect-[5.18] w-[119px]"
+        />
+      </div>
+      <div role="listitem">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/5173ef4683643e5d72e2d299df44f85cc0b1d7ecd76354e240fcfa2be7aeb37d?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+          alt="Partner company logo"
+          class="object-contain shrink-0 rounded-lg aspect-[1.88] w-[184px]"
+        />
+      </div>
+      <div role="listitem">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/7ccc84bbfcfd722c0c97a0999cd14d849265f6236afe7cb8e63a7fe34ca5577c?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+          alt="Partner company logo"
+          class="object-contain shrink-0 rounded-lg aspect-[1.88] w-[184px]"
+        />
+      </div>
+      <div
+        role="listitem"
+        class="flex overflow-hidden flex-col justify-center px-6 py-9 rounded-lg bg-neutral-100 w-[184px] max-md:px-5"
+      >
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/28059154cf7787462972e865f6830f2ef43b297079c4ed485f896f0c0e403c29?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+          alt="Partner company logo"
+          class="object-contain aspect-[4.72] w-[137px]"
+        />
+      </div>
+      <div
+        role="listitem"
+        class="flex overflow-hidden flex-col justify-center px-6 py-9 rounded-lg bg-neutral-100 w-[184px] max-md:px-5"
+      >
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/ea2598d73a540fe638680a51a0117bba73b472788497d1efa9ad0b979365735c?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+          alt="Partner company logo"
+          class="object-contain aspect-[4.72] w-[137px]"
+        />
+      </div>
+      <div
+        role="listitem"
+        class="flex overflow-hidden flex-col justify-center px-6 py-9 rounded-lg bg-neutral-100 w-[184px] max-md:px-5"
+      >
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/b89e451745e832bcf1b051a9e6e785c77660979d7cdd4d1cdf576c7dd95d8d37?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+          alt="Partner company logo"
+          class="object-contain aspect-[4.76] w-[138px]"
+        />
+      </div>
+      <div role="listitem">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/24cca32e8b18e1bee9ed6475ed779ff6e936bb4e1e7212b1fcb2982ddeb33ce8?placeholderIfAbsent=true&apiKey=908bd9b8c3f6460ebb8e726d7a287ccb"
+          alt="Partner company logo"
+          class="object-contain shrink-0 rounded-lg aspect-[1.1] w-[108px]"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
